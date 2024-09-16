88b37fa5e15d77667ef2e1ab8daf7f99d456b4fa
Resha Christian Yanuar Wibowo <56487014+chrisyan20@users.noreply.github.com>
2024-09-16 15:48:38 +0200

    Create backend.py
    Initializing backend.py

---
+++ backend.py
@@ -0,0 +1,21 @@
+# Initializing required modules
+import os
+import sys
+import tkinter as tk
+from tkinter import ttk, messagebox, simpledialog, filedialog, font
+import logging
+import shutil
+
+
+# Defining Object
+class InterfaceDB(tk.Tk):
+  # Constructing Interface
+  def __init__(self):
+    #
+    #
+    #
+
+
+if __name__ == "__main__":
+  app = InterfaceDB()
+  app.mainloop()
