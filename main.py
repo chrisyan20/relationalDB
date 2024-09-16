590ac4951d63f425973e299fb8deba1b90ce5c93
Resha Christian Yanuar Wibowo <56487014+chrisyan20@users.noreply.github.com>
2024-09-16 15:17:10 +0200

    Create main.py
    Initializing main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+# Initializing required modules
+import os
+import sys
+import tkinter as tk
+from tkinter import ttk, messagebox, simpledialog, filedialog, font
+import logging
+import shutil
+from # import AdminLogin, AdminSetup
+from # import InterfaceDB
+
+# Defining Object
+class WelcomingMessage(tk.Tk):
+  # Constructing Interface
+  def __init__(self):
+    #
+    #
+    #
+
+if __name__ == "__main__":
+  app = WelcomingMessage()
+  app.mainloop()
