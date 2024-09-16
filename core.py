7b6d8c280f9e420b51e66c1063f6709b6447447a
Resha Christian Yanuar Wibowo <56487014+chrisyan20@users.noreply.github.com>
2024-09-16 15:45:47 +0200

    Create core.py
    Initializing core.py

---
+++ core.py
@@ -0,0 +1,28 @@
+# Initializing required modules
+import os
+import sys
+import tkinter as tk
+from tkinter import ttk, messagebox, simpledialog, filedialog, font
+import logging
+import shutil
+from # import InterfaceDB
+
+# Defining Object
+class AdminLogin(tk.Tk):
+  # Constructing Interface
+  def __init__(self):
+    #
+    #
+    #
+
+class AdminSetup(tk.Tk):
+  # Constructing Interface
+  def __init__(self):
+    #
+    #
+    #
+
+if __name__ == "__main__":
+  #
+  #
+  
